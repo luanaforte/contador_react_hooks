cb74ce0cde051c2ebf3a874442c1247b2d5ba4f2
luanaforte <luanaforte950@gmail.com>
2024-09-16 22:31:48 -0300

    Adicionando o primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import { useState, useEffect, useRef } from 'react'
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0)
+  const [number, setNumber] = useState(0)
+
+  const numberRef = useRef(0)
+
+  const buttonRef = useRef()
+
+  const oldCountRef = useRef()
+
+  const zerar = () => {
+    if (count !== 0) {
+      setCount(0)
+    }
+  }
+
+  console.log('renderizou')
+
+  console.log(numberRef)
+
+  // não re renderiza o componente
+  useEffect(() => {
+    // setNumber((prevNumber) => prevNumber + 10)   // <- gera loop infinito
+    numberRef.current = Math.random()
+  })
+
+  // referência para elementos de DOM
+  useEffect(() => {
+    console.log(buttonRef.current.click())
+  }, [])
+
+  // referência do valor anterior
+  useEffect(() => {
+    oldCountRef.current = count
+  }, [count])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>o number é: {number}</h1>
+      <h2>O contador anterior era: {oldCountRef.current}</h2>
+      <h1>O contador é: {count}</h1>
+      
+      <button 
+        ref={buttonRef} 
+        onClick={() => setCount((prevCount) => prevCount + 1)}>Adicionar</button>
+
+      <button 
+        ref={buttonRef}
+        onClick={() => setCount((prevCount) => prevCount - 1)}>Retirar</button>
+
+      <button 
+        ref={buttonRef}
+        onClick={zerar}>Zerar</button>
+
+      <h1>o número do useRef é: {numberRef.current}</h1>
     </div>
   );
 }
